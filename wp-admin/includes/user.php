93946b228eb25892739574752552d9f8d8adc6e6
kuba <ttroczynski5@gmail.com>
2022-10-01 13:05:13 +0200

    commit

--- wp-admin/includes/user.php
+++ wp-admin/includes/user.php
@@ -30,7 +30,7 @@ function add_user() {
 function edit_user( $user_id = 0 ) {
 
 
-	var_dump($_SESSION);
+	var_dump($_POST);
 	$wp_roles = wp_roles();
 	$user     = new stdClass;
 	$user_id  = (int) $user_id;
